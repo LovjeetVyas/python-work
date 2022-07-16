c2fc837ff53d200e525c32baa654726cf00a0460
CandyxPoP <luvvyas987@gmail.com>
2022-07-16 13:33:09 +0530

    committed

---
+++ pushthon.py
@@ -0,0 +1,10 @@
+# map_ = {
+#     'first_name':'lovjeet',
+#     'last_name':'vyas'
+# }
+# print(map_['first_name'])
+# print(map_.get('company'))
+# print(map_.get('company','not found'))
+for i in range(2):
+    for j in range(2):
+        print(i,j)
