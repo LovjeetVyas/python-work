3563b819551b0d6f4c0f10a37190550621018417
CandyxPoP <luvvyas987@gmail.com>
2022-07-17 13:12:01 +0530

    commit quit

--- pushthon.py
+++ pushthon.py
@@ -4,7 +4,41 @@
 # }
 # print(map_['first_name'])
 # print(map_.get('company'))
-# print(map_.get('company','not found'))
-for i in range(2):
-    for j in range(2):
-        print(i,j)
+# # print(map_.get('company','not found'))
+# for i in range(2):
+#     for j in range(2):
+#         print(i,j)
+
+
+# fname = 'lovjeet' 
+# lname = 'Vyas' 
+# if fname == 'lovjeet' and lname == 'yadav':
+#     print('true')
+# elif lname == 'Vyas':
+#     print('true')
+# else:
+#     print('not true')
+#     take an input frm the user whose value is even
+# x = input('enter the value : ')
+# for  y in x :
+#         if ord(y)%2==0 :
+#             print (y,'and its ascii value is',ord(y))
+# q2 take 5 input frm the usr and append all those in a list
+
+
+
+# 
+# take n input frm the usr and the input is n*2
+# from typing import Dict
+
+
+x = int(input('enter the no.: '))
+dict_={}
+for z in range(x):
+    data = input('enter data type : ')
+    if data not in dict_:
+        dict_[data]=[]
+    dict_[data].append(input('enter the value : '))    
+print(dict_)
+        
+    
